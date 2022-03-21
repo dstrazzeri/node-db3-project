7dba9f53d929a9f6b343a03e706bc6db81787990
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-03-21 03:06:51 -0400

    forgot to push earlier but all tests passing

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -3,53 +3,47 @@ const Scheme = require('./scheme-model')
 
 
 const checkSchemeId = async (req, res, next) => {
-  try {
-    const data = await db('schemes')
-    .where('scheme_id', req.params.scheme_id)
-    .first()
+    try {
+        const data = await db('schemes')
+            .where('scheme_id', req.params.scheme_id)
+            .first()
 
-    if (!data) {
-      next({
-        status: 404,
-        message: `scheme with scheme)id ${req.params.scheme_id} not found`
-      })
-    } else {
-      next()
+        if (!data) {
+            next({status: 404, message: `scheme with scheme_id ${req.params.scheme_id} not found`})
+        } else {
+            next()
+        }
+    } catch (err) {
+        next(err)
     }
-  } catch (err) {
-    next(err)
-  }
 }
-
 
 const validateScheme = (req, res, next) => {
-  const {scheme_name} = req.body
-  if (scheme_name === undefined || typeof scheme_name !== 'string' || !scheme_name.trim()) {
-    next({
-      status: 400,
-      message: 'invalid scheme_name'
-    })
-  } else {
-    next()
-  }
+    const {scheme_name} = req.body
+    if (scheme_name === undefined || typeof scheme_name !== 'string' || !scheme_name.trim()) {
+        next({
+            status: 400,
+            message: 'invalid scheme_name'
+        })
+    } else {
+        next()
+    }
 }
-
 
 const validateStep = (req, res, next) => {
-  const {instructions, step_number} = req.body
-  if (instructions === undefined || typeof instructions !== 'string' || !instructions.trim() || typeof step_number !== 'number' || step_number < 1) {
-    next({
-      status: 400,
-      message: 'invalid step'
-    })
-  } else {
-    next()
-  }
+    const {instructions, step_number} = req.body
+    if (instructions === undefined || typeof instructions !== 'string' || !instructions.trim() || typeof step_number !== 'number' || step_number < 1) {
+        next({
+            status: 400,
+            message: 'invalid step'
+        })
+    } else {
+        next()
+    }
 }
 
-
 module.exports = {
-  checkSchemeId,
-  validateScheme,
-  validateStep,
+    checkSchemeId,
+    validateScheme,
+    validateStep,
 }
